79af655ffe361738fced96c746245e0bbdf24e8a
Michael <MichaelKerl16@gmail.com>
2018-03-01 03:25:01 -0700

    Create Dummy Repo

---
+++ index.js
@@ -0,0 +1,20 @@
+const react = require('react');
+const styled = require('styled-components');
+
+const Wrapper = styled.div`
+  padding-top: 2rem;
+  padding-bottom: 2rem;
+
+  @media only screen and (max-width : 320px) {
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+  }
+  @media only screen and (min-width : 321px) and (max-width : 1079px) {
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+  }
+  @media only screen  and (min-width : 1080px) and (max-width : 1823px) {
+    padding-top: 1.5rem;
+    padding-bottom: 1.5rem;
+  }
+`;
